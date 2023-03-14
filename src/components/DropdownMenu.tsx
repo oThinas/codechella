19114b6c524c26ada6414ff95a6dab80e28ef323
oThinas <thiagomartinsprado@gmail.com>
2023-03-14 11:21:07 -0300

    fix: dropdown menu não fechava

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,7 +1,5 @@
 import * as Dropdown from '@radix-ui/react-dropdown-menu';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import { Link } from '.';
 
@@ -10,12 +8,6 @@ interface IDropdownMenuProps {
 }
 
 export function DropdownMenu(props: IDropdownMenuProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const { pathname } = useLocation();
-  useEffect(() => {
-    setIsOpen(false);
-  }, [pathname]);
-
   const itemsElement = props.items.map((item, index) => {
     return (
       <Dropdown.Item key={index} className='px-8 py-4 outline-none'>
@@ -33,8 +25,8 @@ export function DropdownMenu(props: IDropdownMenuProps) {
   }
 
   return (
-    <Dropdown.Root open={isOpen}>
-      <Dropdown.Trigger asChild className='outline-none' onClick={() => setIsOpen(true)}>
+    <Dropdown.Root>
+      <Dropdown.Trigger asChild className='outline-none'>
         <button>
           <HamburgerMenuIcon width={32} height={32} color='#FFF'/>
         </button>
